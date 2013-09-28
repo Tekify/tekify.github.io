a76dfe7aad87d2f75b24c41f9c5cb07caa9b125e
pbojinov <petarbojinov@gmail.com>
2013-09-28 16:35:47 -0700

    changed colors of verticals

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+var Tekify = {};
+
+Tekify.Me = function() {
+
+    var $accordian;
+
+    function init() {
+        $accordian = $('.panel');
+        bindEvents();
+    }
+
+    function bindEvents() {
+        $accordian.on('click', function() {
+            //$(this).collapse('toggle');
+        });
+    }
+
+    return {
+        init: init
+    }
+}();
+
+jQuery(document).ready(function() {
+    Tekify.Me.init();
+});
